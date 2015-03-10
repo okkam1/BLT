7583d426b7b660d612c3c1bda0f0603b89a0fbef
okkam1 <okkam1@hotmail.com>
2015-03-10 00:42:41 -0600

    adding default event handlers

--- inetpub/Hobby.aspx.cs
+++ inetpub/Hobby.aspx.cs
@@ -11,4 +11,12 @@ public partial class Hobby : System.Web.UI.Page
     {
 
     }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+
+    }
+    protected void NextButton_Click(object sender, EventArgs e)
+    {
+
+    }
 }
