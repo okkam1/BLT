2a69465a041a3f4248e460c44861d72b0841444b
liamehinc <sysproxy@hotmail.com>
2017-12-28 16:55:00 -0700

    Correct bug where no results return without personID
    code now returns results even if no personID is passed in

--- inetpub/autoCompleteTest.aspx.cs
+++ inetpub/autoCompleteTest.aspx.cs
@@ -24,7 +24,7 @@ public partial class autoCompleteTest : System.Web.UI.Page
         {
             command.Parameters.Add(new SqlParameter("@FirstName", tbFirstName.Text.Trim()));
             command.Parameters.Add(new SqlParameter("@LastName", tbLastName.Text.Trim()));
-            command.Parameters.Add(new SqlParameter("@PersonID", tbPersonID.Text.Trim()));
+            if (tbPersonID.Text.Length > 0) { command.Parameters.Add(new SqlParameter("@PersonID", tbPersonID.Text.Trim())); }
 
             if (ddlYear.SelectedValue != "-")
             {
